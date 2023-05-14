bcf022e3d090e1641343131ce75f9c46c583048a
Suryanarayanamma <119720732+Suryanarayanamma@users.noreply.github.com>
2023-05-14 14:52:32 +0530

    import java.util.*; public class RainDrop {     public static void main(String args[])     {         int n;         String Sound = "";         Scanner sc= new Scanner(System.in);         n = sc.nextInt();         if(n%3 == 0)         Sound = Sound+"Pling";         if(n%5== 0)         Sound = Sound+"Plang";         if(n%7 == 0)         Sound+="Plong";         if(Sound.isEmpty())         System.out.println(n);         else         System.out.println(Sound);         sc.close();     } }

---
+++ RainDrop.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class RainDrop
+{
+    public static void main(String args[])
+    {
+        int n;
+        String Sound = "";
+        Scanner sc= new Scanner(System.in);
+        n = sc.nextInt();
+        if(n%3 == 0)
+        Sound = Sound+"Pling";
+        if(n%5== 0)
+        Sound = Sound+"Plang";
+        if(n%7 == 0)
+        Sound+="Plong";
+        if(Sound.isEmpty())
+        System.out.println(n);
+        else 
+        System.out.println(Sound);
+        sc.close();
+    }
+}
