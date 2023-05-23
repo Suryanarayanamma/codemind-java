7b1a245100899957a93ac64e23debf650b1eaca3
Suryanarayanamma <119720732+Suryanarayanamma@users.noreply.github.com>
2023-05-23 09:14:40 +0530

    import java.util.Scanner; public class value {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         char ch;         int as;         ch=sc.nextLine().charAt(0);         as=(int)ch;         System.out.println(as);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class value
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        char ch;
+        int as;
+        ch=sc.nextLine().charAt(0);
+        as=(int)ch;
+        System.out.println(as);
+    }
+}
